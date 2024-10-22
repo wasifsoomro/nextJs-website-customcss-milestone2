29435411bab00b707d5251b969c54c5cf30f476d
Wasif Soomro <wasifsoomro761@gmail.com>
2024-10-22 16:02:51 +0500

    responsive

--- src/app/(components)/Header.tsx
+++ src/app/(components)/Header.tsx
@@ -1,19 +1,37 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import Styles from "../(components)/Header.module.css"
 import Link from 'next/link';
 
 export default function Header() {
+const [menuOpen, setMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+   setMenuOpen((prevState) => !prevState)
+  }
+
+  
+
   return (
     <header className={Styles.headerContainer}>
       <div className={Styles.logoContainer} id="logo">
         <h1 className={Styles.logo}>WS</h1>
       </div>
 
-      <nav className={Styles.navContainer}>
-        <Link href="/">Home</Link>
-        <Link href="/about">About Me</Link>
-        <Link href="/contact">Contact Me</Link>
+      <div className={Styles.hamburger} onClick={toggleMenu}>
+        <span className={Styles.bar}></span>
+        <span className={Styles.bar}></span>
+        <span className={Styles.bar}></span>
+      </div>
+
+      <nav className={`${Styles.navContainer} ${menuOpen ? Styles.navActive : ''}`}>
+      <div className={Styles.cross} onClick={toggleMenu}>&#10005;</div>
+        <Link href="/" className={Styles.navLink} onClick={toggleMenu}>Home</Link>
+        <Link href="/about" className={Styles.navLink} onClick={toggleMenu}>About Me</Link>
+        <Link href="/contact" className={Styles.navLink} onClick={toggleMenu}>Contact Me</Link>
       </nav>
+      
     </header>
   );
 }
